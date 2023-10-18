13713008c069f4e6b6ea1c75fc3cd4ad7cadfac6
Dawid Birgauan <dbirgauan@student.tgm.ac.at>
2023-10-18 15:38:54 +0200

    4. Commit: In dieser Version wurden die Klassen Main und Rechtschreibtrainer gecodet. Diese Klassen sind noch nicht 100% fertig, sollten aber schon funktionsfähig sein.

--- src/main/java/org/example/Rechtschreibtrainer.java
+++ src/main/java/org/example/Rechtschreibtrainer.java
@@ -1,5 +1,10 @@
 package src.main.java.org.example;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Rechtschreibtrainer {
 
     private int richtig= 0;
@@ -7,7 +12,46 @@ public class Rechtschreibtrainer {
     private static String eingabe;
 
     public int[] openFrame(String name, String url, String text) {
+        JFrame frame= new JFrame("Benutzereingabe");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 200);
+        JPanel panel= new JPanel();
+        panel.setLayout(new BorderLayout());
 
-        return null;
+        JTextArea textArea= new JTextArea(text+url);
+        textArea.setWrapStyleWord(true);
+        textArea.setLineWrap(true);
+        textArea.setOpaque(false);
+        textArea.setEditable(false);
+
+        JScrollPane scrollPane= new JScrollPane(textArea);
+        panel.add(scrollPane, BorderLayout.CENTER);
+
+        JTextField inputField= new JTextField();
+        panel.add(inputField, BorderLayout.SOUTH);
+
+        JButton okButton= new JButton("OK");
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                eingabe= inputField.getText();
+
+                if(eingabe.equals("")) {
+                    textArea.setText("Antwort falsch! Nochmal versuchen: "+url);
+                }
+                if(eingabe.toLowerCase().equals(name.toLowerCase())) {
+                    textArea.setText("Antwort richtig! Weiter mit Enter!");
+                    richtig++;
+                } else {
+                    textArea.setText("Antwort falsch! Nochmal versuchen: "+url);
+                    falsch++;
+                }
+                frame.dispose();
+            }
+        });
+        panel.add(okButton, BorderLayout.EAST);
+        frame.add(panel);
+        frame.setVisible(true);
+        return new int[]{richtig, falsch};
     }
 }

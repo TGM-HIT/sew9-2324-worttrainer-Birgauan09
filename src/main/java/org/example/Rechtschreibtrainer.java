b9a31eac96fcb32120a2c8abd38d142a026a2552
Dawid Birgauan <dbirgauan@student.tgm.ac.at>
2023-10-18 15:31:25 +0200

    3. Commit: In dieser Version wurden alle Fehler im Projekt behoben. Ebenfalls wurde das UML-Diagramm verbessert und die benötigten Klassen wurden erstellt, aber nicht gecodet.

---
+++ src/main/java/org/example/Rechtschreibtrainer.java
@@ -0,0 +1,13 @@
+package src.main.java.org.example;
+
+public class Rechtschreibtrainer {
+
+    private int richtig= 0;
+    private int falsch= 0;
+    private static String eingabe;
+
+    public int[] openFrame(String name, String url, String text) {
+
+        return null;
+    }
+}
